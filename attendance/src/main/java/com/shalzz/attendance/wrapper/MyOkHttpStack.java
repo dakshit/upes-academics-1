3fba7ec9fd2b2cc22e37068c18df3e5100bd997e
Shaleen Jain <shaleen.jain95@gmail.com>
2014-07-13 17:02:50 +0530

    upgraded to okhttp 2.0

--- attendance/src/main/java/com/shalzz/attendance/wrapper/MyOkHttpStack.java
+++ attendance/src/main/java/com/shalzz/attendance/wrapper/MyOkHttpStack.java
@@ -1,21 +1,21 @@
-/*  
- *    Copyright (C) 2013 - 2014 Shaleen Jain <shaleen.jain95@gmail.com>
+/*
+ * Copyright (c) 2014 Shaleen Jain <shaleen.jain95@gmail.com>
  *
- *	  This file is part of UPES Academics.
+ * This file is part of UPES Academics.
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- **/    
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package com.shalzz.attendance.wrapper;
 
@@ -31,36 +31,38 @@ import android.widget.Toast;
 import com.android.volley.toolbox.HurlStack;
 import com.shalzz.attendance.Miscellaneous;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
 
 public class MyOkHttpStack extends HurlStack {
-	private final OkHttpClient client;
+    private final OkUrlFactory okUrlFactory;
 
 	public MyOkHttpStack() {
-	    this(new OkHttpClient());
-	  }
+        this(new OkUrlFactory(new OkHttpClient()));
+    }
 
-	  public MyOkHttpStack(OkHttpClient client) {
-	    if (client == null) {
-	      throw new NullPointerException("Client must not be null.");
-	    }
-	    this.client = client;
-	  }
-	  
-	@Override 
+    public MyOkHttpStack(OkUrlFactory okUrlFactory) {
+        if (okUrlFactory == null) {
+            throw new NullPointerException("Client must not be null.");
+        }
+        this.okUrlFactory = okUrlFactory;
+    }
+
+
+    @Override
 	protected HttpURLConnection createConnection(URL url) throws IOException {
 		if(Miscellaneous.useProxy())
 		{
 			Log.i("MyOkHttpStack","Using Proxy!");
 			Toast.makeText(MyVolley.getAppContext(), "Using Proxy!", Toast.LENGTH_LONG).show();
 			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.ddn.upes.ac.in", 8080));
-			client.setProxy(proxy);
+            okUrlFactory.client().setProxy(proxy);
 		}
-		else if(client.getProxy()!=null)
+		else if(okUrlFactory.client().getProxy()!=null)
 		{
 			Toast.makeText(MyVolley.getAppContext(), "Proxy removed!", Toast.LENGTH_LONG).show();
 			Log.i("MyOkHttpStack","Proxy removed!");
-			client.setProxy(null);
+            okUrlFactory.client().setProxy(null);
 		}
-		return client.open(url);			
+		return okUrlFactory.open(url);
 	} 
 }
