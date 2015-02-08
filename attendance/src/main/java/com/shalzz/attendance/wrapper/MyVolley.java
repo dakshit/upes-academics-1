7003c2156db48d38292d325997863d6f434455fb
Shaleen Jain <shaleen.jain95@gmail.com>
2015-02-09 03:29:00 +0530

    use mipmaps

--- attendance/src/main/java/com/shalzz/attendance/wrapper/MyVolley.java
+++ attendance/src/main/java/com/shalzz/attendance/wrapper/MyVolley.java
@@ -78,7 +78,7 @@ public class MyVolley extends Application {
 		sInstance = this;
 
         Bugsnag.init(this);
-        Bugsnag.setNotifyReleaseStages("production", "testing");
+        Bugsnag.setNotifyReleaseStages("production", "development", "testing");
 		
 		// Set a cookie manager
 		Log.i(MyVolley.class.getName(), "Setting CookieHandler");
