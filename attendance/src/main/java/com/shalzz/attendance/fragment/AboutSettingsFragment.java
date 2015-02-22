57814bcc4082ad6996cbb8a5a6fc01451398e384
Shaleen Jain <shaleen.jain95@gmail.com>
2015-02-22 09:13:56 +0530

    Revert "remove GPL license"
    This reverts commit 0ed0afbff538c8ae04e8d8fb5279cefce2c8c101.

--- attendance/src/main/java/com/shalzz/attendance/fragment/AboutSettingsFragment.java
+++ attendance/src/main/java/com/shalzz/attendance/fragment/AboutSettingsFragment.java
@@ -10,6 +10,10 @@ import com.bugsnag.android.Bugsnag;
 import com.shalzz.attendance.R;
 
 import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.GnuGeneralPublicLicense20;
+import de.psdev.licensesdialog.licenses.GnuGeneralPublicLicense30;
+import de.psdev.licensesdialog.licenses.License;
+import de.psdev.licensesdialog.model.Notice;
 
 public class AboutSettingsFragment extends PreferenceFragment{
 
@@ -29,7 +33,20 @@ public class AboutSettingsFragment extends PreferenceFragment{
         super.onResume();
 
         PreferenceScreen prefScreen =  getPreferenceScreen();
-        Preference noticePref = prefScreen.getPreference(0);
+        Preference pref = prefScreen.getPreference(0);
+        pref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener(){
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                final String name = "UPES Academics";
+                final String url = "http://www.github.com/shalzz/upes-academics";
+                final String copyright = "Copyright (C) 2013 - 2015 Shaleen Jain <shaleen.jain95@gmail.com>";
+                final License license = new GnuGeneralPublicLicense20();
+                final Notice notice = new Notice(name, url, copyright, license);
+                new LicensesDialog.Builder(mContext).setNotices(notice).setShowFullLicenseText(true).build().show();
+                return true;
+            }
+        });
+        Preference noticePref = prefScreen.getPreference(1);
         noticePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener(){
             @Override
             public boolean onPreferenceClick(Preference preference) {
