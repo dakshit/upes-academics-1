7060842b0588d08e1aa3b76353ebcff2c148d815
Shaleen Jain <shaleen.jain95@gmail.com>
2015-02-22 08:42:17 +0530

    Volley: use native OkHttp instead of the HttpURLConnection wrapper

--- attendance/src/main/java/com/shalzz/attendance/wrapper/MyOkHttpStack.java
+++ attendance/src/main/java/com/shalzz/attendance/wrapper/MyOkHttpStack.java
@@ -1,68 +1,185 @@
-/*
- * Copyright (c) 2014 Shaleen Jain <shaleen.jain95@gmail.com>
+/**
+ * The MIT License (MIT)
  *
- * This file is part of UPES Academics.
+ * Copyright (c) 2015 Circle Internet Financial
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
-
 package com.shalzz.attendance.wrapper;
 
-import android.widget.Toast;
-
-import com.android.volley.toolbox.HurlStack;
-import com.bugsnag.android.Bugsnag;
-import com.shalzz.attendance.Miscellaneous;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.toolbox.HttpStack;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.OkUrlFactory;
+import com.squareup.okhttp.Protocol;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.message.BasicStatusLine;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-public class MyOkHttpStack extends HurlStack {
-    private final OkUrlFactory okUrlFactory;
 
-	public MyOkHttpStack() {
-        this(new OkUrlFactory(new OkHttpClient()));
+/**
+ * OkHttp backed {@link com.android.volley.toolbox.HttpStack HttpStack} that does not
+ * use okhttp-urlconnection
+ */
+public class MyOkHttpStack implements HttpStack {
+
+    private final OkHttpClient mClient;
+
+    public MyOkHttpStack() {
+        this(new OkHttpClient());
     }
-
-    public MyOkHttpStack(OkUrlFactory okUrlFactory) {
-        if (okUrlFactory == null) {
-            throw new NullPointerException("Client must not be null.");
-        }
-        this.okUrlFactory = okUrlFactory;
+        
+    public MyOkHttpStack(OkHttpClient client) {
+        this.mClient = client;
     }
-
 
     @Override
-	protected HttpURLConnection createConnection(URL url) throws IOException {
-		if(Miscellaneous.useProxy())
-		{
-            Bugsnag.leaveBreadcrumb("Using Proxy!");
-			Toast.makeText(MyVolley.getAppContext(), "Using Proxy!", Toast.LENGTH_LONG).show();
-			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.ddn.upes.ac.in", 8080));
-            okUrlFactory.client().setProxy(proxy);
-		}
-		else if(okUrlFactory.client().getProxy()!=null)
-		{
-			Toast.makeText(MyVolley.getAppContext(), "Proxy removed!", Toast.LENGTH_LONG).show();
-            Bugsnag.leaveBreadcrumb("Proxy removed!");
-            okUrlFactory.client().setProxy(null);
-		}
-		return okUrlFactory.open(url);
-	} 
-}
+    public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders)
+            throws IOException, AuthFailureError {
+
+        OkHttpClient client = mClient.clone();
+        int timeoutMs = request.getTimeoutMs();
+        client.setConnectTimeout(timeoutMs, TimeUnit.MILLISECONDS);
+        client.setReadTimeout(timeoutMs, TimeUnit.MILLISECONDS);
+        client.setWriteTimeout(timeoutMs, TimeUnit.MILLISECONDS);
+
+        com.squareup.okhttp.Request.Builder okHttpRequestBuilder = new com.squareup.okhttp.Request.Builder();
+        okHttpRequestBuilder.url(request.getUrl());
+
+        Map<String, String> headers = request.getHeaders();
+        for (final String name : headers.keySet()) {
+            okHttpRequestBuilder.addHeader(name, headers.get(name));
+        }
+        for (final String name : additionalHeaders.keySet()) {
+            okHttpRequestBuilder.addHeader(name, additionalHeaders.get(name));
+        }
+
+        setConnectionParametersForRequest(okHttpRequestBuilder, request);
+
+        com.squareup.okhttp.Request okHttpRequest = okHttpRequestBuilder.build();
+        Call okHttpCall = client.newCall(okHttpRequest);
+        Response okHttpResponse = okHttpCall.execute();
+
+        StatusLine responseStatus = new BasicStatusLine(parseProtocol(okHttpResponse.protocol()), okHttpResponse.code(), okHttpResponse.message());
+        BasicHttpResponse response = new BasicHttpResponse(responseStatus);
+        response.setEntity(entityFromOkHttpResponse(okHttpResponse));
+
+        Headers responseHeaders = okHttpResponse.headers();
+        for (int i = 0, len = responseHeaders.size(); i < len; i++) {
+            final String name = responseHeaders.name(i), value = responseHeaders.value(i);
+            if (name != null) {
+                response.addHeader(new BasicHeader(name, value));
+            }
+        }
+
+        return response;
+    }
+
+    private static HttpEntity entityFromOkHttpResponse(Response r) throws IOException {
+        BasicHttpEntity entity = new BasicHttpEntity();
+        ResponseBody body = r.body();
+
+        entity.setContent(body.byteStream());
+        entity.setContentLength(body.contentLength());
+        entity.setContentEncoding(r.header("Content-Encoding"));
+
+        if (body.contentType() != null) {
+            entity.setContentType(body.contentType().type());
+        }
+        return entity;
+    }
+
+    @SuppressWarnings("deprecation")
+    private static void setConnectionParametersForRequest(com.squareup.okhttp.Request.Builder builder, Request<?> request)
+            throws IOException, AuthFailureError {
+        switch (request.getMethod()) {
+            case Request.Method.DEPRECATED_GET_OR_POST:
+                // Ensure backwards compatibility. Volley assumes a request with a null body is a GET.
+                byte[] postBody = request.getPostBody();
+                if (postBody != null) {
+                    builder.post(RequestBody.create(MediaType.parse(request.getPostBodyContentType()), postBody));
+                }
+                break;
+            case Request.Method.GET:
+                builder.get();
+                break;
+            case Request.Method.DELETE:
+                builder.delete();
+                break;
+            case Request.Method.POST:
+                builder.post(createRequestBody(request));
+                break;
+            case Request.Method.PUT:
+                builder.put(createRequestBody(request));
+                break;
+            case Request.Method.HEAD:
+                builder.head();
+                break;
+            case Request.Method.OPTIONS:
+                builder.method("OPTIONS", null);
+                break;
+            case Request.Method.TRACE:
+                builder.method("TRACE", null);
+                break;
+            case Request.Method.PATCH:
+                builder.patch(createRequestBody(request));
+                break;
+            default:
+                throw new IllegalStateException("Unknown method type.");
+        }
+    }
+
+    private static ProtocolVersion parseProtocol(final Protocol p) {
+        switch (p) {
+            case HTTP_1_0:
+                return new ProtocolVersion("HTTP", 1, 0);
+            case HTTP_1_1:
+                return new ProtocolVersion("HTTP", 1, 1);
+            case SPDY_3:
+                return new ProtocolVersion("SPDY", 3, 1);
+            case HTTP_2:
+                return new ProtocolVersion("HTTP", 2, 0);
+        }
+
+        throw new IllegalAccessError("Unkwown protocol");
+    }
+
+    private static RequestBody createRequestBody(Request r) throws AuthFailureError {
+        final byte[] body = r.getBody();
+        if (body == null) return null;
+
+        return RequestBody.create(MediaType.parse(r.getBodyContentType()), body);
+    }
+} 
